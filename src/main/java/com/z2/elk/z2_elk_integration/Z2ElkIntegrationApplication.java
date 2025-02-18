b5e4019fe4a3df20037937ad24a04c76f7310bdd
Krupanandam Muvvala <Krupanandam.muvvala@z2data.com>
2025-02-18 14:04:18 +0530

    Added Required dependencies

---
+++ src/main/java/com/z2/elk/z2_elk_integration/Z2ElkIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.z2.elk.z2_elk_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Z2ElkIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Z2ElkIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/z2/elk/z2_elk_integration/Z2ElkIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z2.elk.z2_elk_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Z2ElkIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
